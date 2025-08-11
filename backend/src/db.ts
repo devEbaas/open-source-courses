fb03caacddfb3821a739ecd9f538c5809db41dfc
Eduardo Baas Kauil <86069086+devEbaas@users.noreply.github.com>
2025-08-10 23:50:15 -0600

    fixed database url

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,17 +1,25 @@
-import mysql from 'mysql2/promise'
+import mysql from "mysql2/promise";
 
-export const pool = mysql.createPool({
-  host: process.env.DB_HOST as string,
-  port: Number(process.env.DB_PORT) || 3306,
-  user: process.env.DB_USER as string,
-  password: process.env.DB_PASSWORD as string,
-  database: process.env.DB_NAME as string,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-})
+const config = process.env.DATABASE_URL
+  ? {
+      uri: process.env.DATABASE_URL,
+    }
+  : {
+      host: process.env.DB_HOST as string,
+      port: Number(process.env.DB_PORT) || 3306,
+      user: process.env.DB_USER as string,
+      password: process.env.DB_PASSWORD as string,
+      database: process.env.DB_NAME as string,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+    };
+
+export const pool = process.env.DATABASE_URL
+  ? mysql.createPool(process.env.DATABASE_URL)
+  : mysql.createPool(config);
 
 export async function testConnection() {
-  const [rows]: any = await pool.query('SELECT 1 as ok')
-  return rows[0].ok === 1
+  const [rows]: any = await pool.query("SELECT 1 as ok");
+  return rows[0].ok === 1;
 }
